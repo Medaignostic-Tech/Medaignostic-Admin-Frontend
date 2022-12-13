897756d820f9806fa54dd8140151a9e57a060081
Shrijeeth S <shrijeethsuresh@gmail.com>
2022-12-13 13:29:01 +0530

    Initialize home and login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App common-background">
-      
+      <BrowserRouter>
+            <Routes>
+                <Route path = "/" exact element={<Home />}></Route> 
+                <Route path = "/login" exact element={<Login /> }></Route>
+            </Routes> 
+        </BrowserRouter>
     </div>
   );
 }
